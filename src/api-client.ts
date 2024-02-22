4f19e9cb92d824b772732eb17d485b096ad975cb
hiep957 <hicharmie1@gmail.com>
2024-02-23 02:10:57 +0700

    two 2am hix

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,66 +1,79 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
 
-
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const register = async (formData: RegisterFormData) => {
-    const response = await fetch(`${API_BASE_URL}/api/users/register`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-  
-    const responseBody = await response.json();
-  
-    if (!response.ok) {
-      console.log("1")
-      console.log(responseBody);
-      throw new Error(responseBody.message);
-    }
-  };
-
-export const SignIn = async(formData: SignInFormData) => {
-  const response = await fetch(`${API_BASE_URL}/api/auth/login`,{
+  const response = await fetch(`${API_BASE_URL}/api/users/register`, {
     method: "POST",
     credentials: "include",
     headers: {
-      "Content-Type":"application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  const responseBody = await response.json();
+
+  if (!response.ok) {
+    console.log("1");
+    console.log(responseBody);
+    throw new Error(responseBody.message);
+  }
+};
+
+export const SignIn = async (formData: SignInFormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(formData),
   });
 
   const body = await response.json();
-  if(!response.ok) {
+  if (!response.ok) {
     throw new Error(body.message);
   }
   return body;
 };
 
+export const validateToken = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+    credentials: "include",
+  });
 
-  export const validateToken = async () => {
-    const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
-      credentials: "include",
-    });
-  
-    if (!response.ok) {
-      console.log(response)
-      throw new Error("Token invalid");
-    }
-  
-    return response.json();
-  };
+  if (!response.ok) {
+    console.log(response);
+    throw new Error("Token invalid");
+  }
+
+  return response.json();
+};
 
 export const signOut = async () => {
-    const response = await fetch(`${API_BASE_URL}/api/auth/logout`, {
-      credentials: "include",
-      method: "POST",
-    });
-  
-    if (!response.ok) {
-      throw new Error("Error during sign out");
-    }
-  };
+  const response = await fetch(`${API_BASE_URL}/api/auth/logout`, {
+    credentials: "include",
+    method: "POST",
+  });
+
+  if (!response.ok) {
+    throw new Error("Error during sign out");
+  }
+};
+
+export const addMyHotel = async (hotelFormData: FormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+    method: "POST",
+    credentials: "include",
+    body: hotelFormData,
+  });
+
+  if (!response.ok) {
+    console.log(response)
+    throw new Error("Failed to add hotel");
+  }
+
+  return response.json();
+};
